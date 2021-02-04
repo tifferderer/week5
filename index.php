22c2bd9c20437f2471e5e7c71f558d62facea8bc
Tiffany Ferderer <tferderer@mail.greenriver.edu>
2021-02-04 14:37:43 -0500

    use of variables shown

--- index.php
+++ index.php
@@ -15,7 +15,14 @@ $f3 = Base::instance();
 $f3->set('DEBUG', 3);
 
 //Define a default route (home page)
-$f3->route('GET /', function() {
+$f3->route('GET /', function($f3) {
+
+    //save variables
+    $f3->set('username', "jshmo");
+    $f3->set('password', sha1('Password01'));
+    $f3->set('title', 'Working with Templates');
+    $f3->set('ftemp', 67);
+
      $view = new Template();
     echo $view->render('view/home.html');
 });
